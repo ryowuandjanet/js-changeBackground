795503b9cd1908e41dea0338b238a8c2b38dfd38
ryowuandjanet <ryowudata@gmail.com>
2019-02-15 00:35:41 +0800

    modify app.js row 11

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const btn=document.getElementById('btn');
 btn.addEventListener('click', function() {
   let random=Math.floor(Math.random()*colors.length);
   const body=document.body;
-  body.style.backgroundColor  = colors[random]
+  body.style.backgroundColor  = colors[random];
 
-  console.log(random());
+  console.log(random);
 });
